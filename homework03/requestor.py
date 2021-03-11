bd7ba82d78237cf2cd7307df8211a8f4e52dd10c
Braden Pecora <bpecora11@gmail.com>
2021-03-10 20:39:47 -0600

    formatted response

--- homework03/requestor.py
+++ homework03/requestor.py
@@ -1,6 +1,9 @@
 import requests
-response = requests.get(url="http://localhost:5026/animals")
+# response = requests.get(url="http://localhost:5026/animals")
+# response = requests.get(url="http://localhost:5026/animals/head/bunny")
+response = requests.get(url="http://localhost:5026/animals/legs/6")
 
-print(response.status_code)
-print(response.json())
-print(response.headers)
+
+print(f'Response Code: {response.status_code} \n')
+print(f'Response: \n {response.json()}')
+print(f'\n Headers:{response.headers}')
